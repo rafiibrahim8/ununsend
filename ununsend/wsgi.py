4c4ccac725657af23be7e8e5159956c2d6dc27a5
Ibrahim Rafi <me@ibrahimrafi.me>
2021-06-22 20:45:06 +0600

    added feature to send all messages

--- ununsend/wsgi.py
+++ ununsend/wsgi.py
@@ -108,7 +108,10 @@ def website_main(active_network=False, port=5000, print_info=[], dbms_parm=None)
         
         fbchatThread = threading.Thread(target=ununsend_client.main, args=(listener, ALWAYS_ACTIVE))
         fbchatThread.start()
-        clearUpThread = threading.Thread(target=utils.clear_up, args=(dbms, CLEANUP_INTERVAL, MAX_MESSAGE_AGE))
+        
+        cleanup_interval = dbms.get_website_stuff('cleanup_interval') or CLEANUP_INTERVAL
+        max_message_age = dbms.get_website_stuff('max_message_age') or MAX_MESSAGE_AGE
+        clearUpThread = threading.Thread(target=utils.clear_up, args=(dbms, cleanup_interval, max_message_age))
         clearUpThread.start()
     
     if active_network:
